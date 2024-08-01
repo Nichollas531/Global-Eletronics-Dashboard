4a4f31ec50b59872c6e1389b8c56df5383963f5d
Nichollas Garcia <72013599+Nichollas531@users.noreply.github.com>
2024-08-01 18:43:26 +0100

    Update Electronics_Project_SQL.sql

--- Electronics_Project_SQL.sql
+++ Electronics_Project_SQL.sql
@@ -1,4 +1,5 @@
-#quantity sold per Country
+# Which country is the leading in sales?
+# quantity sold per Country
 select count(quantity) as Quantity_Sold, country 
 	from sales as s 
 	join stores as t 
@@ -6,20 +7,23 @@ select count(quantity) as Quantity_Sold, country
 	group by country
     order by Quantity_Sold Desc;
 
-#selecting data directly from the name
+# How to query every piece of information from a specific product?
+# selecting data directly from the name
 select * from products where product_name ='Adventure Works Desktop PC2.30 MD230 Black';
 
-#top product name sold; error see *
+# What is the top seller?
+# top product name sold
 select Product_Name,
  SUM(s.quantity * p.Unit_price) as Value_from_Sales
  from products as p join sales as s
  on p.ProductKey = s.ProductKey
  group by Product_name;
 
-
+# Is gender a relevant parameter for sales?
 #total user of genders
 select count(gender) as Total_Users, gender from customers group by gender;
 
+#/-----------------------------------------------------------------------------------/#
 #funny fact people born on a peculiar date
 select count(name), birthday from customers where birthday like '2/29/%' group by birthday;
 
